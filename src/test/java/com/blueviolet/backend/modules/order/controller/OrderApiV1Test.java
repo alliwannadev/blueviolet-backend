486b3f6e9c0a9c2997eed4c2dce2ca26f56002df
alliwannadev <alliwannadev@gmail.com>
2024-04-20 02:56:38 +0900

    test: 주문 관련 테스트 코드 보강 (#13)

--- src/test/java/com/blueviolet/backend/modules/order/controller/OrderApiV1Test.java
+++ src/test/java/com/blueviolet/backend/modules/order/controller/OrderApiV1Test.java
@@ -1,6 +1,7 @@
 package com.blueviolet.backend.modules.order.controller;
 
 import com.blueviolet.backend.common.annotation.IntegrationTest;
+import com.blueviolet.backend.common.dto.OkResponse;
 import com.blueviolet.backend.common.dto.TokenInfo;
 import com.blueviolet.backend.common.error.ErrorCode;
 import com.blueviolet.backend.common.util.AuthUtil;
@@ -13,9 +14,14 @@ import com.blueviolet.backend.modules.category.helper.CategoryTestHelper;
 import com.blueviolet.backend.modules.option.domain.ProductOptionCombination;
 import com.blueviolet.backend.modules.option.helper.ProductOptionCombinationTestHelper;
 import com.blueviolet.backend.modules.order.controller.dto.CreateOrderRequestV1;
+import com.blueviolet.backend.modules.order.controller.dto.CreateOrderResponseV1;
+import com.blueviolet.backend.modules.order.domain.Order;
+import com.blueviolet.backend.modules.order.domain.OrderItem;
+import com.blueviolet.backend.modules.order.helper.OrderTestHelper;
 import com.blueviolet.backend.modules.product.domain.Product;
 import com.blueviolet.backend.modules.product.helper.ProductTestHelper;
 import com.blueviolet.backend.modules.warehousing.helper.ProductWarehousingTestHelper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,9 +29,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -43,6 +51,7 @@ class OrderApiV1Test {
     @Autowired ProductTestHelper productTestHelper;
     @Autowired ProductOptionCombinationTestHelper productOptionCombinationTestHelper;
     @Autowired ProductWarehousingTestHelper productWarehousingTestHelper;
+    @Autowired OrderTestHelper orderTestHelper;
 
     @BeforeEach
     void beforeEach() {
@@ -97,7 +106,7 @@ class OrderApiV1Test {
         String requestBody = jsonUtil.toJson(createOrderRequestV1);
 
         // When & Then
-        mockMvc.perform(
+        String jsonResponse = mockMvc.perform(
                         post(OrderApiPathsV1.V1_ORDERS)
                                 .header(
                                         HttpHeaders.AUTHORIZATION,
@@ -112,7 +121,46 @@ class OrderApiV1Test {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
-                .andExpect(jsonPath("$.message").value("API 요청을 완료하였습니다."));
+                .andExpect(jsonPath("$.message").value("API 요청을 완료하였습니다."))
+                .andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8);
+
+        OkResponse<CreateOrderResponseV1> createOrderResponse =
+                jsonUtil.fromJson(
+                        jsonResponse,
+                        new TypeReference<>() {}
+                );
+        Order order = orderTestHelper.getOneByOrderIdWithRelationship(
+                createOrderResponse.getData().orderId()
+        );
+        assertThat(order.getOrderId()).isEqualTo(createOrderResponse.getData().orderId());
+        assertThat(order.getUserId()).isEqualTo(createOrderResponse.getData().userId());
+        assertThat(order.getOrderNo()).isEqualTo(createOrderResponse.getData().orderNo());
+        assertThat(order.getOrderStatus().getCode()).isEqualTo(createOrderResponse.getData().orderStatus());
+        assertThat(order.getReceiverName()).isEqualTo(createOrderResponse.getData().receiverName());
+        assertThat(order.getReceiverPhoneNumber()).isEqualTo(createOrderResponse.getData().receiverPhoneNumber());
+        assertThat(order.getReceiverAddr()).isEqualTo(createOrderResponse.getData().receiverAddr());
+        assertThat(order.getReceiverPostalCode()).isEqualTo(createOrderResponse.getData().receiverPostalCode());
+        assertThat(order.getTotalAmount()).isEqualTo(createOrderResponse.getData().totalAmount());
+        assertThat(order.getDiscountedTotalAmount()).isEqualTo(createOrderResponse.getData().discountedTotalAmount());
+        assertThat(order.getTotalQuantity()).isEqualTo(createOrderResponse.getData().totalQuantity());
+
+        List<CreateOrderResponseV1.OrderItemDto> orderItemDtoList = createOrderResponse.getData().orderItems();
+        List<OrderItem> orderItems = order.getOrderItems();
+
+        for (int i = 0; i < orderItemDtoList.size(); i++) {
+            CreateOrderResponseV1.OrderItemDto orderItemDto = orderItemDtoList.get(i);
+            OrderItem orderItem = orderItems.get(i);
+
+            assertThat(orderItemDto.orderItemId()).isEqualTo(orderItem.getOrderItemId());
+            assertThat(orderItemDto.productOptionCombinationId()).isEqualTo(orderItem.getProductOptionCombinationId());
+            assertThat(orderItemDto.price()).isEqualTo(orderItem.getPrice());
+            assertThat(orderItemDto.discountedPrice()).isEqualTo(orderItem.getDiscountedPrice());
+            assertThat(orderItemDto.quantity()).isEqualTo(orderItem.getQuantity());
+            assertThat(orderItemDto.subTotalAmount()).isEqualTo(orderItem.getSubTotalAmount());
+            assertThat(orderItemDto.discountedSubTotalAmount()).isEqualTo(orderItem.getDiscountedSubTotalAmount());
+        }
     }
 
     @DisplayName("[API][POST][FAIL] 주문 등록 API 호출 - 잘못된 파라미터를 전달하는 경우")
---
+++ src/test/java/com/blueviolet/backend/modules/order/helper/OrderTestHelper.java
@@ -0,0 +1,23 @@
+package com.blueviolet.backend.modules.order.helper;
+
+import com.blueviolet.backend.common.error.BusinessException;
+import com.blueviolet.backend.common.error.ErrorCode;
+import com.blueviolet.backend.modules.order.domain.Order;
+import com.blueviolet.backend.modules.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class OrderTestHelper {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional(readOnly = true)
+    public Order getOneByOrderIdWithRelationship(Long orderId) {
+        return orderRepository
+                .findOneByOrderIdWithRelationship(orderId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.ORDER_NOT_FOUND));
+    }
+}
